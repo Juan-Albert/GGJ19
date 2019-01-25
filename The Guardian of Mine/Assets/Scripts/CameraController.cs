4b0feebc0c7943b62c48c0c7be6e46e86681717c
Joenoror <falanhx@gmail.com>
2019-01-25 22:02:57 +0100

    Raycast, Camera & GetItem

--- The Guardian of Mine/Assets/Scripts/CameraController.cs
+++ The Guardian of Mine/Assets/Scripts/CameraController.cs
@@ -22,5 +22,32 @@ public class CameraController : MonoBehaviour {
 
             transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
         }
+
+
+
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 9;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1.5f, layerMask))
+        {
+
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+            if (Input.GetMouseButtonDown(0))
+                hit.collider.GetComponent<GetItem>().itemObtained = true;
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
     }
+
 }
--- The Guardian of Mine/Assets/Scripts/GetItem.cs
+++ The Guardian of Mine/Assets/Scripts/GetItem.cs
@@ -11,8 +11,8 @@ public class GetItem : MonoBehaviour
 
     private void Start()
     {
-        mat1 = GetComponent<Material>();
-    }
+        this.GetComponent<Renderer>().material = mat1;
+    } 
 
 
     // Update is called once per frame
@@ -20,11 +20,15 @@ public class GetItem : MonoBehaviour
     {
 
         if (itemObtained)
-            mat1 = mat2;
-            
-
+            this.GetComponent<Renderer>().material = mat2;
+        else
+        {
+            this.GetComponent<Renderer>().material = mat1;
+        }
     }
 
     
+
+
 
 }
