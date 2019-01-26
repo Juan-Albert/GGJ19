abfbe61de0555f91b59b7a5f6dd6f58cd1348c1e
Juan-Albert <juanmartinezlopez96@gmail.com>
2019-01-26 21:04:23 +0100

    Raycast

---
+++ The Guardian of Mine/Assets/Scripts/BorderController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderController : MonoBehaviour
+{
+    public bool changeColor = false;
+    public bool highlightNow = true;
+
+    [ColorUsageAttribute(true, true)]
+    public Color edgeColor;
+
+    private void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (changeColor && highlightNow)
+        {
+            GetComponent<Renderer>().material.SetColor("_ColorSuperficial", edgeColor);
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<Renderer>().material.SetColor("_ColorSuperficial", edgeColor);
+            }
+        }
+        else
+        {
+            GetComponent<Renderer>().material.SetColor("_ColorSuperficial", Color.black);
+
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<Renderer>().material.SetColor("_ColorSuperficial", Color.black);
+            }
+        }
+    }
+}
--- The Guardian of Mine/Assets/Scripts/CameraController.cs
+++ The Guardian of Mine/Assets/Scripts/CameraController.cs
@@ -4,51 +4,41 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    //public float sensitivityY = 15F;
-    //public float minimumY = -60F;
-    //public float maximumY = 60F;
 
-    //float rotationY = 0F;
+    public LayerMask layerMask;
 
-    //public bool movement;
+    private bool detectLastObject = false;
+    private GameObject lastGameObject;
+
+    private RaycastHit hit;
+
+
 
     void Update()
     {
-        //if (movement)
-        //{
+        detectThings();
+    }
 
-        //    rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        //    rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+    private void detectThings()
+    {
 
-        //    transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
-        //}
-
-
-
-        // Bit shift the index of the layer (8) to get a bit mask
-        int layerMask = 9;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        // Does the ray intersect any objects excluding the player layer
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1.5f, layerMask))
         {
-
-            //hit.collider.GetComponent<Renderer>().material = border;
-
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-            Debug.Log("Did Hit");
+            Debug.Log("Hit");
+            //hit.collider.GetComponent<BorderController>().changeColor = true;
+            lastGameObject = hit.collider.gameObject;
+            detectLastObject = true;
             if (Input.GetMouseButtonDown(0))
-                hit.collider.GetComponent<GetItem>().itemObtained = true;
+            {
+                Debug.Log("click");
+            }
 
         }
-        else
+        else if (detectLastObject == true)
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            Debug.Log("Did not Hit");
+            Debug.Log("No Hit");
+            detectLastObject = false;
+            //if (lastGameObject != null) lastGameObject.GetComponent<BorderController>().changeColor = false;
         }
     }
 
