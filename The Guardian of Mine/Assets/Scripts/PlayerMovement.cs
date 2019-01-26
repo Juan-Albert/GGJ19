d7584a92855f0f0b28f90e74dbc848ba1b792895
Juan-Albert <juanmartinezlopez96@gmail.com>
2019-01-26 02:24:25 +0100

    Water movement

--- The Guardian of Mine/Assets/Scripts/PlayerMovement.cs
+++ The Guardian of Mine/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+
+    [Header("Movement")]
     private Rigidbody _rB;
     public float moveSpeed;
     public bool movement;
     public int sensitiveX;
+
+    [Header("Jump")]
+    public bool canJump;
+    public float jumpForce;
+
+    [Header("Ground")]
+    public Transform groundCheck;
+    public Transform reSpawn;
+    public LayerMask whatIsGround;
+
+    private bool _grounded;
+    private bool _jumpKeyPressed;
 
     void Start()
     {
@@ -27,6 +41,13 @@ public class PlayerMovement : MonoBehaviour {
         {
             handleInput();
         }
+
+        if (canJump)
+        {
+            CheckJump();
+        }
+
+        
     }
 
     private void handleInput()
@@ -38,5 +59,37 @@ public class PlayerMovement : MonoBehaviour {
         Vector3 despV = transform.forward * moveSpeed * v * Time.deltaTime;
 
         _rB.MovePosition(_rB.position + despV + despH);
+    }
+
+    private void CheckJump()
+    {
+        if (groundCheck != null)
+        {
+            _grounded = Physics.OverlapSphere(groundCheck.position, 0.01f, whatIsGround).Length > 0;
+        }
+
+        if (_grounded  && Input.GetKeyDown(KeyCode.Space) && _jumpKeyPressed == false)
+        {
+            _jumpKeyPressed = true;
+            _grounded = false;
+            _rB.AddForce(Vector3.up * jumpForce);
+        }
+
+        if (!Input.GetKeyDown(KeyCode.Space) && _jumpKeyPressed == true)
+        {
+            _jumpKeyPressed = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.layer);
+
+        if (collision.gameObject.layer == 10)
+        {
+            Debug.Log("Holi");
+            this.gameObject.transform.position = reSpawn.position;
+
+        }
     }
 }
