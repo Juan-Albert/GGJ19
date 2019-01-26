ca70ce309c22b9e4aed082b97f30e5d1bb160358
Joenoror <falanhx@gmail.com>
2019-01-26 18:07:04 +0100

    Some changes for the cat

---
+++ The Guardian of Mine/Assets/Scripts/CatController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public GameObject parent;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Vector3 targetVector = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+
+        transform.LookAt(targetVector);
+
+        //float diference = parent.transform.eulerAngles.y - transform.eulerAngles.y;
+        //print(diference);
+        //if (diference >= 90 && diference <= 180)
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 90f, transform.eulerAngles.z);
+        //else if(diference > -180 && diference >= -90)
+        //    transform.eulerAngles = new Vector3(transform.eulerAngles.x, -90f, transform.eulerAngles.z);
+
+
+
+        if (transform.eulerAngles.y >= 90 && transform.eulerAngles.y <= 180)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 90f, transform.eulerAngles.z);
+        }
+        else if (transform.eulerAngles.y >= 180 && transform.eulerAngles.y <= 270)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, -90f, transform.eulerAngles.z);
+        }
+    }
+}
