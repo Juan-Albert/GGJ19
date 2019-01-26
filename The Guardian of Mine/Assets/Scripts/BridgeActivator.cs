f352458dc8c366cc1d334a9c050179dea15b0cf2
Joenoror <falanhx@gmail.com>
2019-01-26 16:04:41 +0100

    Changes

---
+++ The Guardian of Mine/Assets/Scripts/BridgeActivator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeActivator : MonoBehaviour
+{
+
+
+
+    public bool itemObtained;
+
+    public Material mat1, mat2;
+
+    public GameObject bridge;
+
+    private void Start()
+    {
+        this.GetComponent<Renderer>().material = mat1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (itemObtained)
+        {
+            this.GetComponent<Renderer>().material = mat2;
+            bridge.GetComponent<Animator>().SetBool("Active", true);
+        }
+
+        else
+        {
+            this.GetComponent<Renderer>().material = mat1;
+        }
+    }
+
+
+
+
+
+}
