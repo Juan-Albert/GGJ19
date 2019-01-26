8d9d6fd96df236a4a1b4890b0cc6c52a18a4769d
Claverias4 <joaquinclaverias@gmail.com>
2019-01-26 19:11:46 +0100

    "Added Fixed shader and final level logic"

---
+++ The Guardian of Mine/Assets/PedestalController.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PedestalController : MonoBehaviour
+{
+    public GameObject[] obj;
+    public GameObject book;
+
+    public GameObject firstPersonController;
+
+    public Material illuminated;
+    public Material dissolution;
+    public Material illuminationBorder;
+
+    private bool inTrigger = false;
+    private bool giveMeBook = false;
+    private bool inChange = false;
+    private bool dissolve_obj = false;
+
+    public float changeTime = 5f;
+    public float time_ratio = 1f;
+    private float not_dis = 0.5f;
+    private float dis = -0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        illuminated.SetFloat("_width", 1f);
+        dissolution.SetFloat("_dissolve",0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dissolve_obj)
+        {
+            not_dis -= Time.deltaTime / time_ratio;
+            if (not_dis <= dis)
+            {
+                not_dis = -0.5f;
+                obj[0].SetActive(false);
+                obj[1].SetActive(false);
+                obj[2].SetActive(false);
+                book.SetActive(true);
+                GetBook();
+            }
+                
+            dissolution.SetFloat("_dissolve", not_dis);
+        }
+        else if (giveMeBook)
+        {
+            dis += Time.deltaTime / time_ratio;
+            if (dis >= 0.5f)
+            {
+                dis = 0.5f;
+                ReturnControl();
+            }
+
+            dissolution.SetFloat("_dissolve", dis);
+        }
+        else
+        {
+            if (inTrigger && Input.GetMouseButtonDown(1) && !dissolve_obj)
+            {
+                ChangeMaterial();
+                dissolve_obj = true;
+                DissolveObj();
+            }
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            illuminated.SetFloat("_width", 0.1f);
+            inTrigger = true;
+        }   
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            illuminated.SetFloat("_width", 1f);
+            inTrigger = false;
+        }
+    }
+
+    void DissolveObj()
+    {
+        firstPersonController.GetComponent<FirstPersonController>().enabled = false;
+    }
+
+    void ReturnControll()
+    {
+        firstPersonController.GetComponent<FirstPersonController>().enabled = true;
+    }
+
+    void GetBook()
+    {
+        giveMeBook = true;
+        dissolve_obj = false;
+    }
+
+    void ChangeMaterial()
+    {
+        foreach(GameObject g in obj)
+        {
+            g.GetComponent<MeshRenderer>().material = dissolution;
+        }
+        
+    }
+}
---
+++ The Guardian of Mine/Assets/RuntimeMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeMaterial : MonoBehaviour
+{
+
+    public float currentExposure = 0.5f;
+    public Material targetMaterial;
+	
+	void Update ()
+    {
+        targetMaterial.SetFloat("Vector1_A1674467", currentExposure);
+
+    }
+}
