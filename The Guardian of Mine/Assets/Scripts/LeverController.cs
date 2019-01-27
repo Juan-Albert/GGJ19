2af2e5a8978654e4a32dcbaa5e5f299e256078c2
Joenoror <falanhx@gmail.com>
2019-01-27 14:10:27 +0100

    Medieval

--- The Guardian of Mine/Assets/Scripts/LeverController.cs
+++ The Guardian of Mine/Assets/Scripts/LeverController.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class LeverController : MonoBehaviour
 {
+
+    AudioSource aS;
+
     public GameObject bridge;
+
+    private void Start()
+    {
+        aS = GetComponent<AudioSource>();
+    }
 
     public void Activate()
     {
         bridge.GetComponent<Animator>().SetBool("Active", true);
+        aS.Play();
     }
 }
