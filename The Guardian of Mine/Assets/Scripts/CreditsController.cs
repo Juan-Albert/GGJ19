5fae21b6e9c0487afa03e0745d397a61eb64c8b2
Claverias4 <joaquinclaverias@gmail.com>
2019-01-27 15:28:35 +0100

    "Added Credits"

---
+++ The Guardian of Mine/Assets/Scripts/CreditsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsController : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetTrigger("Start");
+    }
+
+    public void RestartToMainScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
