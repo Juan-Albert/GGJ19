0ae45b71be11fcf80c1db2c7c22ee51fe398b04a
Joenoror <falanhx@gmail.com>
2019-01-27 12:08:45 +0100

    Camera & Menu

---
+++ The Guardian of Mine/Assets/CameraManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraManager : MonoBehaviour
+{
+    public Animator myTransicion;
+    public float time, transitionTime;
+    private float finalTime, finalTransTime;
+
+    public Camera mainCamera;
+
+    public List<Camera> cameras;
+
+    private int i = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalTime = time;
+        mainCamera = cameras[i];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finalTime -= Time.deltaTime;
+        //finalTransTime -= Time.deltaTime;
+
+        //if (finalTransTime <= 0)
+        //{
+        //    StartCoroutine(myLoadFade());
+
+        //    finalTransTime = transitionTime;
+
+        //}
+
+        
+        if (finalTime <= 0)
+        {
+            i++;
+            
+            if (i == 3)
+            {
+                i = 0;
+            }
+            mainCamera = cameras[i];
+            finalTime = time;
+        }
+
+        
+        if (i==0)
+        {
+            cameras[0].gameObject.SetActive(true);
+            cameras[1].gameObject.SetActive(false);
+            cameras[2].gameObject.SetActive(false);
+        }
+        else if (i == 1)
+        {
+            cameras[0].gameObject.SetActive(false);
+            cameras[1].gameObject.SetActive(true);
+            cameras[2].gameObject.SetActive(false);
+        }
+        else if (i == 2)
+        {
+            cameras[0].gameObject.SetActive(false);
+            cameras[1].gameObject.SetActive(false);
+            cameras[2].gameObject.SetActive(true);
+        }
+    }
+
+
+        
+    
+
+    //IEnumerator myLoadFade()
+    //{
+       
+    //    myTransicion.SetTrigger("end");
+    //    yield return new WaitForSeconds(10f);
+        
+    //}
+}
---
+++ The Guardian of Mine/Assets/CameraRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public GameObject terrain;
+
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector3.Distance(this.transform.position, terrain.transform.position);
+
+        Debug.Log(distance);
+
+        transform.RotateAround(terrain.transform.position, new Vector3(0,1,0), 20 * Time.deltaTime * speed);
+
+
+    }
+}
