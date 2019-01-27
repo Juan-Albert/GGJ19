475ad0dc71e38611263e30fc8cdab8b3b2352a3d
Juan-Albert <juanmartinezlopez96@gmail.com>
2019-01-27 14:16:56 +0100

    Medieval Pirate Final Levels

---
+++ The Guardian of Mine/Assets/Scripts/SpaceDoor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceDoor : MonoBehaviour
+{
+
+    public GameObject knob;
+
+    public bool inside = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && inside)
+        {
+            Debug.Log("COlision");
+            knob.GetComponent<DoorKnob>().Activate();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            knob.GetComponent<BorderController>().changeColor = true;
+            inside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            knob.GetComponent<BorderController>().changeColor = false;
+            inside = false;
+        }
+    }
+}
