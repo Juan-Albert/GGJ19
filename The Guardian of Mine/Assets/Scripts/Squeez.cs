99bdb8d99859433a2c663d76fb20a17b7674fbf9
Juan Albert Martínez López <juanmartinezlopez96@gmail.com>
2019-04-28 14:06:51 +0200

    primer nivel arreglado

---
+++ The Guardian of Mine/Assets/Scripts/Squeez.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Squeez : MonoBehaviour
+{
+    public AudioSource squeez;
+ 
+
+
+     void  OnMouseOver()
+    {
+       float myPitch = Random.Range(1f,1.1f);
+
+        squeez.pitch = myPitch;
+
+        if (Input.GetMouseButtonDown(0)){
+
+            squeez.Play();
+           
+        }
+    }
+
+
+}
